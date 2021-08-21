7913ab2f57b36692a478d68b9df1aaeb4cd00670
bluerhythmz <73447805+bluerhythmz@users.noreply.github.com>
2021-08-21 15:59:26 -0700

    logic

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,100 +1,146 @@
 import React from "react";
-import dollarSign from '../images/icon-dollar.svg'
-import person from '../images/icon-person.svg'
+import dollarSign from "../images/icon-dollar.svg";
+import person from "../images/icon-person.svg";
 import ValueDisplay from "./ValueDisplay";
 import { useState } from "react";
 
 const Form = () => {
-    const [bill, setBill] = useState('')
-    const [people, setPeople] = useState('')
-    const [tipPercent, setTipPercent] = useState(0)
-    const [customTipPercent, setCustomTipPercent] = useState(0)
-    const percents = [5, 10, 15, 25, 50]
-    const [activeButton, setActiveButton] = useState('')
+  const [bill, setBill] = useState("0");
+  const [people, setPeople] = useState("");
+  const [tipPercent, setTipPercent] = useState(0);
+  const [customTipPercent, setCustomTipPercent] = useState("");
+  const [activeButton, setActiveButton] = useState("");
+  const [error, setError] = useState(false);
+  const percents = [5, 10, 15, 25, 50];
 
-    const handleClick = (e) => {
-      const value = parseInt(e.target.value)
-      if (value < 10) {
-        setTipPercent(`${0}${value}`)
-        return setActiveButton(value)
-      }
-      setTipPercent(value)
-      setActiveButton(value)
+  const handleClick = (e) => {
+    if (e.target.value < 10) {
+      setTipPercent(`${0}${e.target.value}`);
+      setCustomTipPercent("");
+      return setActiveButton(e.target.value);
     }
+    setTipPercent(e.target.value);
+    setCustomTipPercent("");
+    setActiveButton(e.target.value);
+  };
 
-    const handleBillChange = (e) => {
-        const value = parseInt(e.target.value)
-        if (e.target.value === '') {
-            return setBill(0)
-        }
-        setBill(value)
+  const handleBillChange = (e) => {
+    if (e.target.value === "") {
+      return setBill("0");
     }
+    setBill(e.target.value);
+  };
 
-    const handlePeopleChange = (e) => {
-      const value = parseInt(e.target.value)
-      /* if (e.target.value === '') {
-        return setPeople(0)
-      } */
-      setPeople(value)
-      }
-
-    const handleCustomTipChange = (e) => {
-      /* if (typeof e.target.value !== "number") return */
-      const value = parseInt(e.target.value)
-      /* if (e.target.value === '') {
-        return setTipPercent(0)
-      } */
-      if (value === 0) {
-        return setCustomTipPercent('')
-      }
-      if (value < 10) {
-        return setCustomTipPercent(`0${value}`)
-      }
-
-      setCustomTipPercent(value)
+  const handlePeopleChange = (e) => {
+    if (e.target.value === "") {
+      setError(true);
+      return setPeople("0");
     }
+    setError(false);
+    setPeople(e.target.value);
+  };
 
-    const handleReset = () => {
-      setBill(0)
-      setTipPercent(0)
+  const handleCustomTipChange = (e) => {
+    if (e.target.value === 0) {
+      return setCustomTipPercent("");
     }
+    if (e.target.value < 10) {
+      setActiveButton("");
+      setTipPercent(0);
+      return setCustomTipPercent(`0${e.target.value}`);
+    }
+    setTipPercent(0);
+    setActiveButton("");
+    setCustomTipPercent(e.target.value);
+  };
 
-    const tipAmount = bill *  `.${tipPercent}`
-    const customTipAmount = bill *  `.${customTipPercent}`
-    const total = people ? (bill + tipAmount) / people : 0 
-    const customTipTotal = people ? (bill + customTipAmount) / people : 0 
+  const handleReset = () => {
+    setBill(0);
+    setTipPercent(0);
+    setCustomTipPercent("");
+    setPeople("");
+    setActiveButton("");
+    setError("");
+  };
+
+  let tipAmount = parseInt(bill) * `.${parseInt(tipPercent)}`;
+  let customTipAmount = parseInt(bill) * `.${parseInt(customTipPercent)}`;
+  let total = people ? (parseInt(bill) + tipAmount) / parseInt(people) : 0;
+  let customTipTotal =
+    people && customTipAmount
+      ? (parseInt(bill) + customTipAmount) / parseInt(people)
+      : 0;
 
   return (
     <main>
       <div className="form">
         <div className="input-group">
           <label htmlFor="bill" className="form__label">
-              Bill
-            </label>
-            <div className="input-wrapper">
-                <input type="text" name="bill" id="bill" value={bill} onChange={handleBillChange} className="form__input --bill" />
-                <img src={dollarSign} alt="dollar-sign" className="input-icon" />
-            </div>
-            <label htmlFor="" className="form__label">Select Tip %</label>
-            <div className="grid">
-              {percents.map(percent => {
-                  const activeClass = activeButton === percent ? "active" : ""
-                  return (
-                    <button key={percent} onClick={handleClick} value={percent} className={`tip-button ${activeClass}`}>{percent}%</button>
-                  )
-              })}
-              <input type="text" name="tip" id="tip" className="form__input custom-tip" value={customTipPercent ? customTipPercent : ''} onChange={handleCustomTipChange} placeholder="Custom" />
-            </div>
-            <label htmlFor="bill" className="form__label">
-              Number Of People
-            </label>
-            <div className="input-wrapper">
-                <div className="error">Can't be zero</div>
-                <input type="text" dir="rtl" name="number-of-people" id="number-of-people" onChange={handlePeopleChange} value={people} className="form__input --people" />
-                <img src={person} alt="person" className="input-icon" />
-            </div>
+            Bill
+          </label>
+          <div className="input-wrapper">
+            <input
+              type="text"
+              name="bill"
+              id="bill"
+              value={bill}
+              onChange={handleBillChange}
+              className={`form__input --bill ${bill === "0" ? "no-bill" : ""}`}
+            />
+            <img src={dollarSign} alt="dollar-sign" className="input-icon" />
+          </div>
+          <label htmlFor="" className="form__label">
+            Select Tip %
+          </label>
+          <div className="grid">
+            {percents.map((percent) => {
+              const activeClass =
+                activeButton === percent.toString() ? "active" : "";
+              return (
+                <button
+                  key={percent}
+                  onClick={handleClick}
+                  value={percent}
+                  className={`tip-button ${activeClass}`}
+                >
+                  {percent}%
+                </button>
+              );
+            })}
+            <input
+              type="text"
+              name="tip"
+              id="tip"
+              className="form__input custom-tip"
+              value={customTipPercent ? customTipPercent : ""}
+              onChange={handleCustomTipChange}
+              placeholder="Custom"
+            />
+          </div>
+          <label htmlFor="bill" className="form__label">
+            Number Of People
+          </label>
+          <div className="input-wrapper">
+            <div className={error ? "error" : "no-error"}>Can't be zero</div>
+            <input
+              type="text"
+              dir="rtl"
+              name="number-of-people"
+              id="number-of-people"
+              onChange={handlePeopleChange}
+              value={people}
+              className={`form__input --people ${error ? "error-border" : ""}`}
+            />
+            <img src={person} alt="person" className="input-icon" />
+          </div>
         </div>
-        <ValueDisplay handleReset={handleReset} total={total} customTipTotal={customTipTotal} tipAmount={tipAmount} customTipAmount={customTipAmount} />
+        <ValueDisplay
+          handleReset={handleReset}
+          total={total}
+          customTipTotal={customTipTotal}
+          tipAmount={tipAmount}
+          customTipAmount={customTipAmount}
+        />
       </div>
     </main>
   );
--- src/components/ValueDisplay.js
+++ src/components/ValueDisplay.js
@@ -1,26 +1,47 @@
-
-
-const ValueDisplay = ({ total, customTipTotal, tipAmount, customTipAmount, handleReset }) => {
-    let noTotal = total ? false : true
-    return (
-        <div className="display">
-            <div className="display__row">
-                <div className="display__textbox">
-                    <div className="display__text">Tip Amount</div>
-                    <div className="display__subtext">/ person</div>
-                </div>
-                <div className="display__value">${customTipAmount ? customTipAmount.toFixed(2) : tipAmount.toFixed(2)}</div>
-            </div>
-            <div className="display__row">
-                <div className="display__textbox">
-                    <div className="display__text">Total</div>
-                    <div className="display__subtext">/ person</div>
-                </div>
-                <div className="display__value">${customTipTotal ? customTipTotal.toFixed(2) : total.toFixed(2)}</div>
-            </div>
-            <button type="submit" onClick={handleReset} className="submit-btn" disabled={noTotal} >Reset</button>
+const ValueDisplay = ({
+  total,
+  customTipTotal,
+  tipAmount,
+  customTipAmount,
+  handleReset,
+}) => {
+  let noTotal = total ? false : true;
+  return (
+    <div className="display">
+      <div className="display__row">
+        <div className="display__textbox">
+          <div className="display__text">Tip Amount</div>
+          <div className="display__subtext">/ person</div>
         </div>
-    )
-}
+        <div className="display__value">
+          $
+          {customTipAmount
+            ? customTipAmount.toFixed(2)
+            : tipAmount.toFixed(2)}
+        </div>
+      </div>
+      <div className="display__row">
+        <div className="display__textbox">
+          <div className="display__text">Total</div>
+          <div className="display__subtext">/ person</div>
+        </div>
+        <div className="display__value">
+          $
+          {customTipTotal
+            ? customTipTotal.toFixed(2)
+            : total.toFixed(2)}
+        </div>
+      </div>
+      <button
+        type="submit"
+        onClick={handleReset}
+        className="submit-btn"
+        disabled={noTotal}
+      >
+        Reset
+      </button>
+    </div>
+  );
+};
 
-export default ValueDisplay
+export default ValueDisplay;
